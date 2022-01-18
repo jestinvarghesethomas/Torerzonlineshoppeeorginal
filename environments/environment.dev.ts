e968e3f541728547abd0e91290c5e46596abf6b1
jestinvarghesethomas <97930603+jestinvarghesethomas@users.noreply.github.com>
2022-01-18 21:11:41 +0530

    Add files via upload

---
+++ environments/environment.dev.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production: false,
+    CUSTOMER:{
+
+        BASE_URL:"http://localhost:4200/# ",
+        BASE_CUSTOMER_URL:"http://localhost:4200/registration",
+        
+        GET_ALL_CUSTOMERS:'list',
+        
+        CREATE_CUSTOMER:'create',
+        UPDATE_CUSTOMER:'update',
+        DELETE_CUSTOMER:'delete'
+        }
+  };
+  
